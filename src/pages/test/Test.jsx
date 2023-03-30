5596207420f0678c10998945827b76bcc0b078a7
zimoscop <zimoscop@gmail.com>
2023-03-30 11:41:22 +0300

    fix:style and add LS

--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,139 +1,129 @@
-import { createRef, useState } from 'react';
-import './Test.css';
+/* eslint-disable quotes */
+/* eslint-disable indent */
+// eslint-disable-next-line import/order
+import { createRef, useState, useEffect } from "react";
+import "./Test.css";
 
 import {
-	getTelegramBotName,
-	getTelegramMessages,
-	sendTelegramMessage,
-} from '@services/telegramAPI';
+  getTelegramBotName,
+  getTelegramMessages,
+  sendTelegramMessage,
+  // eslint-disable-next-line import/no-unresolved, import/newline-after-import
+} from "@services/telegramAPI";
+
+let saveResponseId = JSON.parse(localStorage.getItem("responseid")) ?? [];
 
 function Test() {
+  const inputRef = createRef();
+  const textareaRef = createRef();
+  const [teleName, setTeleName] = useState("");
+  const [teleMessages, setTeleMessages] = useState([]);
 
-	const inputRef = createRef();
-	const textareaRef = createRef();
-	const [teleName, setTeleName] = useState('');
-	const [teleMessages, setTeleMessages] = useState([]);
+  const [responseid, setResponseId] = useState(saveResponseId);
 
-	const teleNameUrl = teleName && `https://t.me/${teleName}`;
+  useEffect(() => {
+    localStorage.setItem("responseid", JSON.stringify(responseid));
+  }, [responseid]);
 
-	const getName = () => {
-		const token = inputRef.current?.value;
-		getTelegramBotName(token).then((readyData) => {
-			console.log(' >1> ', readyData);
-			setTeleName(readyData.result.username);
-		});
-	};
+  const teleNameUrl = teleName && `https://t.me/${teleName}`;
 
-	const getMessages = () => {
-		const token = inputRef.current?.value;
-		getTelegramMessages(token).then((readyData) => {
-			console.log(' >2> ', readyData);
-			setTeleMessages(readyData.result.map((update) => update.message));
-		});
-	};
+  const getName = () => {
+    const token = inputRef.current?.value;
+    getTelegramBotName(token).then((readyData) => {
+      console.log(" >1> ", readyData);
+      setTeleName(readyData.result.username);
+      setResponseId([...responseid, { id: readyData.result.id }]);
+    });
+  };
 
-	const doGreet = (userId) => {
-		const token = inputRef.current?.value;
-		const messageText = textareaRef.current?.value;
-		console.log(textareaRef.current.value);
-		sendTelegramMessage(
-			token,
-			{
-				chat_id: userId,
-				text: messageText,
-			}
-		).then((readyData) => {
-			console.log(' >3> ', readyData);
-			setTeleMessages(readyData.result.map((update) => update.message));
-		});
-	};
+  const getMessages = () => {
+    const token = inputRef.current?.value;
+    getTelegramMessages(token).then((readyData) => {
+      console.log(" >2> ", readyData);
+      setTeleMessages(readyData.result.map((update) => update.message));
+    });
+  };
 
-	return (
-		<div className="Test">
-			<a href={'/'}>&laquo; back</a>
-			<div>
-        piu test
-			</div>
-			<ol>
+  const doGreet = (userId) => {
+    const token = inputRef.current?.value;
+    const messageText = textareaRef.current?.value;
+    console.log(textareaRef.current.value);
+    sendTelegramMessage(token, {
+      chat_id: userId,
+      text: messageText,
+    }).then((readyData) => {
+      console.log(" >3> ", readyData);
+      setTeleMessages(readyData.result.map((update) => update.message));
+    });
+  };
 
-				<li>
-          Create a Telegram-bot here:
-					{' '}
-					<a
-						href="https://t.me/botfather"
-					>
-            https://t.me/botfather
-					</a>
-				</li>
+  return (
+    <div className="Test">
+      <a href={"/"}>&laquo; back</a>
+      <div>piu test</div>
+      <ol className="test_box">
+        <li>
+          Create a Telegram-bot here:{" "}
+          <a href="https://t.me/botfather">https://t.me/botfather</a>
+        </li>
 
-				<li>
+        <li>
           Enter Token:
-					<form>
-						<div>
-							<input
-								ref={inputRef}
-								className="Test__input"
-								placeholder="Token to access the HTTP API"
-								type="text"
-							/>
-						</div>
-					</form>
-				</li>
+          <form>
+            <div>
+              <input
+                ref={inputRef}
+                className="Test__input"
+                placeholder="Token to access the HTTP API"
+                type="text"
+              />
+            </div>
+          </form>
+        </li>
 
-				<li>
+        <li>
           Push the button:
-					<div>
-						<button
-							className="Test__button"
-							onClick={getName}
-						>
+          <div>
+            <button className="Test__button" onClick={getName}>
               Say my name
-						</button>
-						<br />
-						{teleName && (
-							<a href={teleNameUrl}>{teleNameUrl}</a>
-						)}
-					</div>
-				</li>
+            </button>
+            <br />
+            {teleName && <a href={teleNameUrl}>{teleNameUrl}</a>}
+          </div>
+        </li>
 
-				<li>
+        <li>
           Push the next button:
-					<div>
-						<button
-							className="Test__button"
-							onClick={getMessages}
-						>
+          <div>
+            <button className="Test__button" onClick={getMessages}>
               Get messages
-						</button>
-					</div>
-					{teleMessages.map((message, index) => (
-						<div key={index}>
-							<h3>{message.text}</h3>
-							<sup>
-								{message.from.first_name}
-							</sup>
-							<input
-								type="button"
-								value="Greet"
-								onClick={() => {
-									doGreet(message.from.id);
-								}}
-							/>
-						</div>
-					))}
-				</li>
+            </button>
+          </div>
+          {teleMessages.map((message, index) => (
+            <div key={index}>
+              <h3>{message.text}</h3>
+              <sup>{message.from.first_name}</sup>
+              <input
+                type="button"
+                value="Greet"
+                onClick={() => {
+                  doGreet(message.from.id);
+                }}
+              />
+            </div>
+          ))}
+        </li>
 
-				<li>
-
-					<textarea
-						ref={textareaRef}
-						placeholder="Greetings"
-					>
-					</textarea>
-				</li>
-			</ol>
-		</div>
-	);
+        <li>
+          <textarea
+            className="test__textarea"
+            ref={textareaRef}
+            placeholder="Greetings"
+          ></textarea>
+        </li>
+      </ol>
+    </div>
+  );
 }
 
 export default Test;
