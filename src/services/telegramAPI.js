29f3873736dbd3eedc2363b2646b5137c7e6ced2
mudaston <twil.revaas@gmail.com>
2023-03-31 18:13:22 +0300

    format with eslint

--- src/services/telegramAPI.js
+++ src/services/telegramAPI.js
@@ -1,27 +1,25 @@
-/* eslint-disable quotes */
-/* eslint-disable indent */
 export const getTelegramBotName = (token) => {
-  return fetch(`https://api.telegram.org/bot${token}/getMe`).then((data) => {
-    return data.json();
-  });
+	return fetch(`https://api.telegram.org/bot${token}/getMe`).then((data) => {
+		return data.json();
+	});
 };
 
 export const getTelegramMessages = (token) => {
-  return fetch(`https://api.telegram.org/bot${token}/getUpdates`).then(
-    (data) => {
-      return data.json();
-    }
-  );
+	return fetch(`https://api.telegram.org/bot${token}/getUpdates`).then(
+		(data) => {
+			return data.json();
+		}
+	);
 };
 
 export const sendTelegramMessage = (token, data) => {
-  return fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).then((data) => {
-    return data.json();
-  });
+	return fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	}).then((data) => {
+		return data.json();
+	});
 };
--- src/telebots/BasicBot.js
+++ src/telebots/BasicBot.js
@@ -1,64 +1,64 @@
 export class BasicBot {
-  static settings = {
-    intervalTime: 10000,
-  }
+	static settings = {
+		intervalTime: 10000,
+	};
 
-  static isProcessed(message, processedIdArr) {
-    return processedIdArr.includes(message.id);
-  }
+	static isProcessed(message, processedIdArr) {
+		return processedIdArr.includes(message.id);
+	}
 
-  constructor (initSettings) {
-    const {
-      onSendCallback,
-      saveProcessedMessageId,
-      getProcessedMessagesId,
-      getTelegramMessages,
-      sendTelegramMessage,
-      botName,
-    } = initSettings;
+	constructor(initSettings) {
+		const {
+			onSendCallback,
+			saveProcessedMessageId,
+			getProcessedMessagesId,
+			getTelegramMessages,
+			sendTelegramMessage,
+			botName,
+		} = initSettings;
 
-    this.onSendCallback = onSendCallback;
-    this.saveProcessedMessageId = saveProcessedMessageId;
-    this.getProcessedMessagesId = getProcessedMessagesId;
-    this.getTelegramMessages = getTelegramMessages;
-    this.sendTelegramMessage = sendTelegramMessage;
+		this.onSendCallback = onSendCallback;
+		this.saveProcessedMessageId = saveProcessedMessageId;
+		this.getProcessedMessagesId = getProcessedMessagesId;
+		this.getTelegramMessages = getTelegramMessages;
+		this.sendTelegramMessage = sendTelegramMessage;
 
-    this._processedIds = [];
-    this._interval = null;
-    this.botName = botName || "BasicBot";
-  }
+		this._processedIds = [];
+		this._interval = null;
+		this.botName = botName || 'BasicBot';
+	}
 
-  async _doWork () {
-    console.log(' > ', new Date());
-    try {
-      this._processedIds = this.getProcessedMessagesId(this.botName);
-      const messages = await this.getTelegramMessages();
-      messages
-        .filter((message) => !BasicBot.isProcessed(message, this._processedIds))
-        .forEach((message) => this._sendResponse(message));
-    } catch (e) {
-      console.log(e);
-    }
-  }
+	async _doWork() {
+		console.log(' > ', new Date());
+		try {
+			this._processedIds = this.getProcessedMessagesId(this.botName);
+			const messages = await this.getTelegramMessages();
+			messages
+				.filter((message) => !BasicBot.isProcessed(message, this._processedIds))
+				.forEach((message) => this._sendResponse(message));
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-  async _sendResponse(message) {
-    await this.sendTelegramMessage('hi!', message.from.id);
-    this._onSend(message);
-  }
+	async _sendResponse(message) {
+		await this.sendTelegramMessage('hi!', message.from.id);
+		this._onSend(message);
+	}
 
-  start() {
-    this.interval = setInterval(this._doWork, BasicBot.settings.intervalTime);
-  }
+	start() {
+		this.interval = setInterval(this._doWork, BasicBot.settings.intervalTime);
+	}
 
-  stop() {
-    clearInterval(this.interval);
-  }
+	stop() {
+		clearInterval(this.interval);
+	}
 
-  _onSend(message) {
-    if (this.onSendCallback) {
-      this.onSendCallback(message);
-      this.saveProcessedMessageId(this.botName, message.id); // id?
-    }
-  }
+	_onSend(message) {
+		if (this.onSendCallback) {
+			this.onSendCallback(message);
+			this.saveProcessedMessageId(this.botName, message.id); // id?
+		}
+	}
 
 }
