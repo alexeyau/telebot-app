e0fd01795e7925f035858642690614b1ef99b809
Peter Yershov <pitnkz1@gmail.com>
2023-04-01 22:55:44 +0700

    without locale storage

--- src/pages/checkMessage/CheckMessage.jsx
+++ src/pages/checkMessage/CheckMessage.jsx
@@ -1,8 +1,27 @@
 import { useEffect, useState } from "react";
 
 export default function CheckMessage({ token }) {
-  // token = "6041598672:AAF6yC3jRKDPAsfSWr8yJVulXl6zCak5jBc";
+  token = "6041598672:AAF6yC3jRKDPAsfSWr8yJVulXl6zCak5jBc";
   const arrReadMessages = [];
+
+  function getRandomNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  const sendMessage = (userId, getRandomArbitrary) => {
+    fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chat_id: userId,
+        text: getRandomNum(0, 10),
+      }),
+    });
+  };
 
   const getMessages = (token) => {
     fetch(`https://api.telegram.org/bot${token}/getUpdates`)
@@ -13,6 +32,7 @@ export default function CheckMessage({ token }) {
         readyData.result.map((update) => {
           if (!arrReadMessages.includes(update.message.message_id)) {
             arrReadMessages.push(update.message.message_id);
+            sendMessage(update.message.from.id);
           }
         });
       });
