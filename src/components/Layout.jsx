9ce3d87ede73c4fa242327741f3e80f04937353f
Ildar <ildar.latipov@gmail.com>
2023-05-27 14:49:15 +0200

    clean zustand

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,19 +4,11 @@ import './Components.css';
 
 function Layout(props) {
   return (
-<<<<<<< HEAD
     <div className='Layout'>
       <Header />
-      {props.children}
-=======
-    <div className="Layout">
-      <Header />
       <div className='page'>
-        <div className='content'>
-          {props.children}
-        </div>
+        <div className='content'>{props.children}</div>
       </div>
->>>>>>> develop
       <Footer />
     </div>
   );
--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,14 +1,5 @@
 import { createRef, useState, useEffect } from 'react';
 
-import { shallow } from 'zustand/shallow';
-import { useBotStore } from '@/services/zustand/store/useBotStore';
-// const { amount, title } = useBotStore(
-//   (state) => ({ amount: state.amount, title: state.title }),
-//   shallow,
-// );
-// const updateAmount = useBotStore((state) => state.updateAmount);
-// console.log(amount, title);
-
 import {
   getTelegramBotName,
   getTelegramMessages,
@@ -16,63 +7,44 @@ import {
 } from '@services/telegramAPI.js';
 import { getStorageItem, setStorageItem } from '@services/localStorage.js';
 import { BasicBotRandom } from '@/telebots/BasicBotRandom';
-<<<<<<< HEAD
-
-=======
 import { BasicBot } from '@/telebots/BasicBot';
->>>>>>> develop
 import Layout from '@/components/Layout';
 
 import './Test.css';
 
 let saveResponseId = JSON.parse(localStorage.getItem('responseid')) ?? [];
 
-<<<<<<< HEAD
-function Test() {
+let saveToStorage = (event) => {
+  setStorageItem('actualKey', event.target.value);
+};
+
+function Test(props) {
   const inputRef = createRef();
   const textareaRef = createRef();
   const [teleName, setTeleName] = useState('');
   const [teleMessages, setTeleMessages] = useState([]);
-=======
-let saveToStorage = (event) => {
-	setStorageItem("actualKey", event.target.value)
-};
-
-function Test(props) {
-
-	const inputRef = createRef();
-	const textareaRef = createRef();
-	const [teleName, setTeleName] = useState('');
-	const [teleMessages, setTeleMessages] = useState([]);
->>>>>>> develop
 
   const [responseid, setResponseId] = useState(saveResponseId);
 
-<<<<<<< HEAD
   useEffect(() => {
+    if (getStorageItem('actualKey').length < 1) console.log('entire token');
     localStorage.setItem('responseid', JSON.stringify(responseid));
   }, [responseid]);
-=======
-	useEffect(() => {
-		if(getStorageItem("actualKey").length < 1) console.log("entire token")
-		localStorage.setItem('responseid', JSON.stringify(responseid));
-	}, [responseid]);
->>>>>>> develop
 
   const teleNameUrl = teleName && `https://t.me/${teleName}`;
 
-<<<<<<< HEAD
   const getName = () => {
+    if (getStorageItem('actualKey').length < 1) return;
     const token = inputRef.current?.value;
     getTelegramBotName(token).then((readyData) => {
       console.log(' >1> ', readyData);
       setTeleName(readyData.result.username);
       setResponseId([...responseid, { id: readyData.result.id }]);
     });
-    console.log(token, settings);
   };
 
   const getMessages = () => {
+    if (getStorageItem('actualKey').length < 1) return;
     const token = inputRef.current?.value;
     getTelegramMessages(token).then((readyData) => {
       console.log(' >2> ', readyData);
@@ -81,6 +53,7 @@ function Test(props) {
   };
 
   const doGreet = (userId) => {
+    if (getStorageItem('actualKey').length < 1) return;
     const token = inputRef.current?.value;
     const messageText = textareaRef.current?.value;
     console.log(textareaRef.current.value);
@@ -94,6 +67,7 @@ function Test(props) {
   };
 
   const createBotInstance = () => {
+    if (getStorageItem('actualKey').length < 1) return;
     const botName = 'botName001';
     const token = inputRef.current?.value;
     const settings = {
@@ -109,7 +83,6 @@ function Test(props) {
       },
       getProcessedMessagesIds: () => {
         const botData = JSON.parse(getStorageItem(botName) || '{}');
-
         return botData.processedMessagesIds || [];
       },
       getTelegramMessagesAsync: async () => {
@@ -134,113 +107,27 @@ function Test(props) {
   return (
     <Layout>
       <div className='Test'>
-        <a href={'/'}>&laquo; back</a>
-        <div>piu test</div>
+        <a href={'/'}>&laquo;</a>
         <ol className='test_box'>
           <li>
             Create a Telegram-bot here: <a href='https://t.me/botfather'>https://t.me/botfather</a>
           </li>
-=======
-	const getName = () => {
-		if(getStorageItem("actualKey").length < 1) return;
-		const token = inputRef.current?.value;
-		getTelegramBotName(token).then((readyData) => {
-			console.log(' >1> ', readyData);
-			setTeleName(readyData.result.username);
-			setResponseId([...responseid, { id: readyData.result.id }]);
-		});
-	};
 
-	const getMessages = () => {
-		if(getStorageItem("actualKey").length < 1) return;
-		const token = inputRef.current?.value;
-		getTelegramMessages(token).then((readyData) => {
-			console.log(' >2> ', readyData);
-			setTeleMessages(readyData.result.map((update) => update.message));
-		});
-	};
-
-	const doGreet = (userId) => {
-		if(getStorageItem("actualKey").length < 1) return;
-		const token = inputRef.current?.value;
-		const messageText = textareaRef.current?.value;
-		console.log(textareaRef.current.value);
-		sendTelegramMessage(token, {
-			chat_id: userId,
-			text: messageText,
-		}).then((readyData) => {
-			console.log(' >3> ', readyData);
-			setTeleMessages(readyData.result.map((update) => update.message));
-		});
-	};
-
-	const createBotInstance = () => {
-		if(getStorageItem("actualKey").length < 1) return;
-		const botName = 'botName001';
-		const token = inputRef.current?.value;
-		const settings = {
-			name: botName,
-			saveProcessedMessageId: (mId) => {
-				// todo move JSON to services methods
-				const botData = JSON.parse(getStorageItem(botName) || '{}');
-				const nextBotData = JSON.stringify({
-					...botData,
-					processedMessagesIds: [
-						...(botData.processedMessagesIds || []),
-						mId,
-					],
-				});
-				setStorageItem(botName, nextBotData);
-			},
-			getProcessedMessagesIds: () => {
-				const botData = JSON.parse(getStorageItem(botName) || '{}');
-				return botData.processedMessagesIds || [];
-			},
-			getTelegramMessagesAsync: async () => {
-				return getTelegramMessages(token).then((readyData) => {
-					return readyData.result.map(update => update.message);
-				});
-			},
-			sendTelegramMessageAsync: async (userId, messageText) => {
-				return sendTelegramMessage(token, {
-					chat_id: userId,
-					text: messageText,
-				});
-			},
-			onSendCallback: () => {
-				console.log('callback: message sent');
-			}
-		};
-		const bot = new BasicBotRandom(settings);
-		bot.start();
-	};
-
-	return (
-		<Layout>
-			<div className='Test'>
-				<a href={'/'}>&laquo;</a>
-				<ol className='test_box'>
-					<li>
-						Create a Telegram-bot here:{' '}
-						<a href='https://t.me/botfather'>https://t.me/botfather</a>
-					</li>
-
-					<li>
-						Enter Token:
-						<form>
-							<div>
-								<input
-									onChange={saveToStorage}
-									ref={inputRef}
-									className='Test__input'
-									placeholder='Token to access the HTTP API'
-									type='text'
-									defaultValue={getStorageItem("actualKey")}
-								/>
-							</div>
-						</form>
-					</li>
->>>>>>> develop
+          <li>
+            Enter Token:
+            <form>
+              <div>
+                <input
+                  onChange={saveToStorage}
+                  ref={inputRef}
+                  className='Test__input'
+                  placeholder='Token to access the HTTP API'
+                  type='text'
+                  defaultValue={getStorageItem('actualKey')}
+                />
+              </div>
+            </form>
+          </li>
 
           <li>
             Enter Token:
