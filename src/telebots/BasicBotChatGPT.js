3d85219295804645aa556288af886be2d10d32ca
Alexey YA <your@email.example>
2023-06-06 11:37:35 +0400

    poetry by word

--- src/telebots/BasicBotChatGPT.js
+++ src/telebots/BasicBotChatGPT.js
@@ -10,17 +10,18 @@ export class BasicBotChatGPT extends BasicBot {
   }
 
   async _sendResponse(update) {
-    const answer = await this._askChatGPT();
+    const answer = await this._askChatGPT(update);
     await this.sendTelegramMessageAsync(update.message?.from.id, answer);
     this._onSend(update);
   }
 
-  async _askChatGPT() {
+  async _askChatGPT(update) {
     if (!this.chatGPTKey?.length > 0) {
       return 'No ChatGPT key';
     }
 
     const question = `Напиши, пожалуйста, четверостишие на английском
+      со словом ${update.message?.text},
       с использованием лексики уровня A2.
       В ответе покажи только стихи.`;
 
